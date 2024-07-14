165529ccda85e04a4f5aa66d60a5b59c9c530621
moohaanaad <mohanadahmed266@gmail.com>
2024-07-14 22:36:00 +0300

    remove cors

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import express from 'express'
 import bootstrap from './src/bootstrap.js'
 const app = express()
-app.use(cors())
+// app.use(cors())
 const port = process.env.port || 5000 
 bootstrap(app, express)
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+/**
+ * company model >>> hr << job model 
+
+ */
